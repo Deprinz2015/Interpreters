a41163f4d8f09e1fb9ede7bec28d0fcd68add1f7
Niklas Koll <nikkol567@gmail.com>
2024-09-08 21:57:41 +0200

    Note for gc

---
+++ zox/src/vm/GC.zig
@@ -0,0 +1,5 @@
+// Try reference counting:
+// Keep track of all objects by type and a reference count
+// expose two functions: countUp, countDown
+// pass object to count to these functions
+// on countDown, when count reaches 0 dealloc object
